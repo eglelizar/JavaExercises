dd2175b937feddd947b76c929fec5b15367b3578
Elizabeth Razo <eglelizar@gmail.com>
2020-10-26 22:10:44 -0700

    10_19_Inboxing

---
+++ HelloWorldFirst/src/Generics/FootballPlayer.java
@@ -0,0 +1,11 @@
+package Generics;
+
+public class FootballPlayer extends Player{
+
+	public FootballPlayer(String name) {
+		super(name);
+		
+	}
+	
+
+}
--- HelloWorldFirst/src/Generics/Main.java
+++ HelloWorldFirst/src/Generics/Main.java
@@ -5,13 +5,32 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> items = new ArrayList<Integer>();
+		FootballPlayer joe = new FootballPlayer("Joe");
+		BaseballPlayer pat = new BaseballPlayer("Pat");
+		SoccerPlayer beckman = new SoccerPlayer ("Beckham");
+		
+		Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+		adelaideCrows.addPlayer(joe);
+		//adelaideCrows.addPlayer(pat);
+		//adelaideCrows.addPlayer(beckman);
+			
+		
+		System.out.println(adelaideCrows.numPlayers());
+				
+		Team<BaseballPlayer> baseballTeam = new Team <> ("Chicago Cubs");
+		baseballTeam.addPlayer(pat);
+		
+		Team<SoccerPlayer> brokenTeam = new Team <> ("this won't work");
+		brokenTeam.addPlayer(beckman);
+		
+		
+		/*ArrayList<Integer> items = new ArrayList<Integer>();
 		items.add(1);
 		items.add(2);
 		items.add(3);
 		items.add(4);
 		items.add(5);
-		printDouble (items);
+		printDouble (items);*/
 	}
 	
 	private static void printDouble(ArrayList<Integer> n) {
